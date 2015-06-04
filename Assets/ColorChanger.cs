690d3ffad47ba948e85d74aa9c3762a51491dcbe
jscotty <jscotty@hotmail.nl>
2015-06-04 19:06:23 +0200

    added fading to prototype

---
+++ Assets/ColorChanger.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ColorChanger : MonoBehaviour {
+
+	private SpriteRenderer _spriteRen;
+
+	private float r = 1f,g = 1f,b = 1f;
+
+	void Start () {
+		_spriteRen = GetComponent<SpriteRenderer>();
+	}
+
+	void Update () {
+		if(Input.GetKey(KeyCode.I)){
+			r -= 0.01f;
+			g -= 0.01f;
+			b -= 0.01f;
+		} else if(Input.GetKey(KeyCode.K)){
+			r += 0.01f;
+			g += 0.01f;
+			b += 0.01f;
+		}
+		
+		Color32 color = new Color(r,g,b,225f);
+		_spriteRen.color = color;
+
+
+	}
+}
