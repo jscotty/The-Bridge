e662f4e390df2511240079f76a995fc37cc27d35
jscotty <jscotty@hotmail.nl>
2015-06-04 18:04:42 +0200

    boat animation and player movement

---
+++ Assets/scripts/BoatAnim.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BoatAnim : MonoBehaviour {
+
+	[SerializeField]
+	private float _speed;
+	
+	[SerializeField]
+	private float _moveX = 1;
+
+	private Rigidbody2D _body;
+
+	void Start () {
+		_body = GetComponent<Rigidbody2D>();
+	}
+
+	void Update () {
+		Move();
+	}
+
+	private void Move(){
+		//boat movement with moveVelocity
+		Vector2 moveVel = _body.velocity; 
+		moveVel.x = _moveX;
+		moveVel.y = 0;
+		_body.velocity = moveVel * _speed * 0.5f;
+
+		Rotate();
+	}
+	private void Rotate(){
+		//print(transform.position.x + " | movex " + _moveX);
+		//1.3 \\ -0.9
+		if(transform.position.x >= 1.3f){
+			_moveX -= 0.01f;
+		} else if(transform.position.x <= -0.9f){
+			_moveX += 0.01f;
+			
+		}
+
+		Scale();
+	}
+	
+	private void Scale(){
+
+		if(_moveX > 0){ // face right (scaleX -1)
+			transform.localScale = new Vector2(-1f,1f);
+		} else if(_moveX < 0){ // face left (scale 1)
+			transform.localScale = new Vector2(1f,1f);
+		}
+	}
+}
---
+++ Assets/scripts/PlayerMovement.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField]
+	private int _speed;
+
+	private Rigidbody2D _body;
+	
+	private int _jumpSpeed = 1;
+	private int _jumpCount;
+
+	void Start () {
+		_body = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Move();
+		Rotate();
+	}
+
+	private void Move(){
+		Vector2 moveVel = _body.velocity;
+		moveVel.x = Input.GetAxis(Methods.HORIZONTAL);
+		moveVel.y = 0;
+
+
+		if(Input.GetKey(KeyCode.Space)){
+			moveVel.y = _jumpSpeed;
+			_jumpCount++;
+			Jump();
+		}
+		_body.velocity = moveVel * _speed;
+	}
+	private void Jump(){
+		if(_jumpCount >= 15 || Input.GetKeyUp(KeyCode.Space)){
+			_jumpSpeed = 0;
+		}
+	}
+	private void OnCollisionEnter2D(Collision2D other){
+		_jumpCount = 0;
+		_jumpSpeed = 1;
+	}
+	private void Rotate(){
+		Vector2 rot = new Vector2(transform.localScale.x,transform.localScale.y);
+		rot.y = 0.3f;
+
+		float x = Input.GetAxisRaw(Methods.HORIZONTAL);
+
+		if(x > 0f){
+			rot.x = 0.3f;
+		} else if(x < 0f){
+			rot.x = -0.3f;
+		}
+
+		transform.localScale = rot;
+	}
+}
