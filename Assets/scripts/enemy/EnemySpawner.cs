fc235500fcaad1f7335bf393361f1a9272c6378c
Floris <florisdh@gmail.com>
2015-06-10 09:36:17 +0200

    First merge
    - Begin Enemy Spawn

---
+++ Assets/scripts/enemy/EnemySpawner.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemySpawner : MonoBehaviour
+{
+    #region Vars
+
+    [SerializeField]
+    private EnemySpawnInfo[] _enemySpawnInfo;
+
+    #endregion
+
+    #region Methods
+
+    private void Start()
+    {
+    }
+
+    private void Update()
+    {
+
+    }
+
+    private void SpawnNext()
+    {
+        byte rnd = (byte)(Random.value * 100);
+        Debug.Log(rnd);
+    }
+
+    #endregion
+
+    #region Properties
+
+    #endregion
+}
+
+[System.Serializable]
+public struct EnemySpawnInfo
+{
+    public GameObject Enemy; // Prefab
+    public short chance; // (0 to 99) %
+}
--- Assets/scripts/references/Levels.cs
+++ Assets/scripts/references/Levels.cs
@@ -5,4 +5,5 @@ public class Levels : MonoBehaviour {
 
 	public const string MAIN_MENU = "mainMenu";
 	public const string GAME_JUSTIN = "mainsceneJustin";
+    public const string GAME_FLORIS = "mainsceneFloris";
 }
